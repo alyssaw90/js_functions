a46d6cd9f66ab0edbf7c9f6f2a1cbb985319a0c7
Alyssa <alyssaw90@gmail.com>
2014-11-19 22:42:11 -0800

    Finished for tonight

--- google_shopping_func.js
+++ google_shopping_func.js
@@ -1,5 +1,5 @@
 //load products.json
-var data = require("./products.json")
+// var data = require("./products.json")
 
 //define getItemsCount function
 //accepts full item data
@@ -9,7 +9,70 @@ var getItemsCount = function(itemData){
 };
 
 //output item count using the getItemsCount function
-console.log('Item Count: ' + getItemsCount(data));
+// console.log('Item Count: ' + getItemsCount(data));
 
 //create your answer in this file.
 //the above is just provided as a simple example.
+var googleProducts = require("./products.json")
+
+
+console.log("Problem 1")
+var getItems = function(data) {
+	return data.items
+}
+
+console.log(getItems(googleProducts))
+
+
+console.log("Problem 2")
+var newArray = [];
+
+var getItemByBrand = function(array,company) {
+	for (var i = 0; i < array.length; i++) {
+	if (array[i].product.brand === company) {
+		newArray.push(array[i]);
+		}
+	}
+
+	return newArray
+}
+
+console.log(getItemByBrand(getItems(googleProducts), "Sony"))
+
+console.log("Problem 3")
+var authorArray = []
+
+var getItemsByAuthor = function(array,author) {
+	for (var i = 0; i < array.length; i++) {
+		if (array[i].product.author.name === author) {
+			authorArray.push(array[i])
+		}
+	}
+
+	return authorArray
+}
+
+console.log(getItemsByAuthor(getItems(googleProducts), "CDW"))
+
+console.log("Problem 4")
+var availableArray = []
+
+var getAvailableProducts = function(array,status) {
+	for (var i = 0; i < array.length; i++) {
+	var inventoriesLength = array[i].product.inventories.legnth;
+	for (var j = 0; j < inventoriesLength; j++){
+		if (array[i].product.inventories[j].availability === "inStock") {
+			 console.log(i + " " + array[i].product.googleId + ": " + array[i].product.inventories[j].availability);
+			availableArray.push(array[i].product)
+		} 
+	}
+}
+
+	return availableArray
+}
+
+console.log(getAvailableProducts(getItems()));
+
+
+
+
